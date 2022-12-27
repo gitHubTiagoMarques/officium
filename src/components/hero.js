f74311092f5d1cec1b98cac4a54957e8a811dea3
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-27 21:11:23 +0000

    hero mobile done

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,13 @@
+import image from "../assets/hero.png"
 export function Hero (){
     return(
         <section className={"hero"}>
-            <img src="../../assets/hero.png"></img>
+            <img src={image}/>
             <h1 className={'title'}>WHY PORTUGAL?</h1>
             <h2 className={"sub"}>There are lots of benefits.</h2>
+            <button className={"call"}>Learn More</button>
+            <p>or</p>
+            <h6><u>Find a Job</u></h6>
 
         </section>
             
