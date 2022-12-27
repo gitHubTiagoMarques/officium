4b6bdd80322902c0dda0df530d7be1fbbb512adb
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-27 23:54:52 +0000

    hero done

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,12 +3,23 @@ export function Hero (){
     return(
         <section className={"hero"}>
             <img src={image}/>
+            <div className="all">
+                <div className={"col-sm-3 why"}>
             <h1 className={'title'}>WHY PORTUGAL?</h1>
             <h2 className={"sub"}>There are lots of benefits.</h2>
+            </div>
+            <div className="calldiv">
             <button className={"call"}>Learn More</button>
             <p>or</p>
             <h6><u>Find a Job</u></h6>
+            </div>
+            </div>
+            <img className="deskimg d-none" src={image}/>
 
+            
+ 
+            
+            
         </section>
             
         )
