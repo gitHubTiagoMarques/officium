fcd95a2d01b2354836a1711b6105ca5d98d9a1a3
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-27 20:01:54 +0000

    hero 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Navbar} from './components/navbar.js'
 import {Footer} from './components/footer.js'
+import {Hero} from './components/hero.js'
 
 function App() {
   return (
     <div className="App">
         <Navbar></Navbar>
+        <Hero></Hero>
         <Footer></Footer>
     </div>
   );
---
+++ src/components/hero.js
@@ -0,0 +1,11 @@
+export function Hero (){
+    return(
+        <section className={"hero"}>
+            <img src="../../assets/hero.png"></img>
+            <h1 className={'title'}>WHY PORTUGAL?</h1>
+            <h2 className={"sub"}>There are lots of benefits.</h2>
+
+        </section>
+            
+        )
+}
