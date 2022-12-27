b99589ff38ce4fd3f103e8c95b723d05d744a9e2
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-27 01:45:12 +0000

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+export function Footer (){
+    return(
+        <footer>
+<div>
+                <div className={'footer'}>OFFICIUM 2022</div>
+            </div>
+        </footer>
+            
+        )
+}
