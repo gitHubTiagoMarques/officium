fd78f462ba32d7fe82b1ab225284f10f33b173d2
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-28 18:09:52 +0000

    carrossel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/navbar.js'
 import {Footer} from './components/footer.js'
 import {Hero} from './components/hero.js'
-import { Carro } from './components/carro';
+import { Carro } from './components/carro.js';
 
 function App() {
   return (
--- src/components/carro.js
+++ src/components/carro.js
@@ -1,13 +1,43 @@
 import um from "../assets/carro1.png"
-export function Carro (){
-    return(
-        <section className={"carro"}>
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+class Carro extends Component {
+
+    render(){
+        return(
+
+        <Carousel className={"text-white showArrows={true} onChange={onChange} onClickItem={onClickItem}"}>
+            <div className={"carro"}>
             <div className={"col-sm-7 carroinfo"}>
             <h2 className={'titlecarro'}>Affordable cost of living</h2>
-            <img src={um}/>
+            <img className={"imgmobile"} src={um}/>
             <p className={"subcarro"}>According to the Cost of Living Index, Portugal's cost of living is 67% lower than that of the United States of America.</p>
             </div>
             <img className="carroimg d-none" src={um}/>
-        </section>   
+            </div>
+        <div className={"carro"}>
+            <div className={"col-sm-7 carroinfo"}>
+            <h2 className={'titlecarro'}>Affordable cost of living</h2>
+            <img className={"imgmobile"} src={um}/>
+            <p className={"subcarro"}>According to the Cost of Living Index, Portugal's cost of living is 67% lower than that of the United States of America.</p>
+            </div>
+            <img className="carroimg d-none" src={um}/>
+        </div>   
+        <div className={"carro"}>
+            <div className={"col-sm-7 carroinfo"}>
+            <h2 className={'titlecarro'}>Affordable cost of living</h2>
+            <img className={"imgmobile"} src={um}/>
+            <p className={"subcarro"}>According to the Cost of Living Index, Portugal's cost of living is 67% lower than that of the United States of America.</p>
+            </div>
+            <img className="carroimg d-none" src={um}/>
+            </div>
+        </Carousel>
+  
         )
+    }
 }
+
+export {Carro}
