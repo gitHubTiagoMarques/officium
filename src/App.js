76f61004234cb59e4e8a920291b7ee1824a97017
cerkeira <ricardocerqueira0@protonmail.com>
2022-12-28 00:56:44 +0000

    clean logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import {Navbar} from './components/navbar.js'
 import {Footer} from './components/footer.js'
 import {Hero} from './components/hero.js'
+import { Carro } from './components/carro';
 
 function App() {
   return (
     <div className="App">
         <Navbar></Navbar>
         <Hero></Hero>
+        <Carro></Carro>
         <Footer></Footer>
     </div>
   );
---
+++ src/components/carro.js
@@ -0,0 +1,7 @@
+export function Carro (){
+    return(
+        <section className={"carro"}>
+        
+        </section>   
+        )
+}
