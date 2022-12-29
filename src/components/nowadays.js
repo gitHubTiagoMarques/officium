29f317dc609461bf1d6359f3d8557383bf5be3a4
githubTiagoMarques <al.tiagosoares@gmail.com>
2022-12-29 02:34:09 +0000

    comecei a ultima parte da homepage v2

--- src/components/nowadays.js
+++ src/components/nowadays.js
@@ -3,12 +3,15 @@ export function Nowadays (){
     return (
         <section className={'row nowadays'}>
             <div className={'col-sm-12 col-md-6'}>
-                <h1 className={'titlecarro'}>Nowadays... It’s different</h1>
+                <h1 className={'titlecarro'}>Nowadays... <br/> It’s different</h1>
                 <p className={'subnowadays'}>It used to be that living in the cities was the best option for people looking for new job opportunities or to pursue a career in any new market, like tech.</p>
                 <p className={'subnowadays'}>But nowadays, it's different. Several factors made it that there are a lot of advantages to seeking a career path in Portugal's rural places.</p>
             </div>
+            <div className={'col-sm-12 col-md-4 imgnowadays'}>
+                <img className={'nowadaysimg'} src={image}/>
+            </div>
             <div className={'col-sm-12 col-md-4'}>
-                <img className={'um'} src={image}/>
+                <h1>sgasdgas gafr gadrf</h1>
             </div>
         </section>
     )
