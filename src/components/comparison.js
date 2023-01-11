0afd13e8866e5c4dcd4c87023974eca2e900254a
cerkeira <ricardocerqueira0@protonmail.com>
2023-01-11 10:57:09 +0000

    idk

--- src/components/comparison.js
+++ src/components/comparison.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { Compare } from './compare';
 import { useState } from 'react';
+import index from '../assets/index.json'
 
-
+const Index = index;
 
 
    const  Comparison = () => {
@@ -23,7 +22,62 @@ import { useState } from 'react';
 <input className={"input"} type={'text'} placeholder='Country' id='country' name='country' onChange={e =>handleChange(e.target.value)}></input>
 </div>
 <hr/>
-<Compare></Compare>
+            <div className={"compare"}>
+                    <div className={"two"}>
+                    <div className={"one"}>
+                    <p>teste</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>teste</p>
+                    </div>
+                    <div className={"one"}>
+                    <p>100</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>100</p>
+                    </div>
+                    </div>
+                    <div className={"two"}>
+                    <div className={"one"}>
+                    <p>111</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>111</p>
+                    </div>
+                    <div className={"one"}>
+                    <p>fá</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>sol</p>
+                    </div>
+                    </div>
+                    <div className={"two"}>
+                    <div className={"one"}>
+                    <p>111</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>111</p>
+                    </div>
+                    <div className={"one"}>
+                    <p>fá</p>
+                    <div className={'ryze'}>
+                        <p className={'legend'}>Quality of life index</p>
+                    <hr></hr>
+                    </div>
+                    <p>sol</p>
+                    </div>
+                    </div>
+                </div>
             </div>
         )
     }
