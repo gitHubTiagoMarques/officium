7bf4eb5165ba4962207e6f788e71325faa611168
cerkeira <ricardocerqueira0@protonmail.com>
2023-01-11 02:15:37 +0000

    trying to make input work

--- src/components/comparison.js
+++ src/components/comparison.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import index from '../assets/index.json'
-const Index = index;
+import { Compare } from './compare';
+import { useState } from 'react';
 
-class Comparison extends Component{
-    render(){
+
+
+
+   const  Comparison = () => {
+
+        const [country, setCountry] = useState();
+        const handleChange= (e) =>{
+            setCountry(e);
+        }
+        console.log(country);
         return(
             <div className={"comparison snap"}>
                 <h1>Compare the quality of life</h1>
@@ -12,73 +20,13 @@ class Comparison extends Component{
 <div className={"countries"}>
 <input className={"portugal"} type={'text'} value='Portugal'></input>
 <hr></hr>
-<input className={"input"} type={'text'} placeholder='Country'></input>
+<input className={"input"} type={'text'} placeholder='Country' id='country' name='country' onChange={e =>handleChange(e.target.value)}></input>
 </div>
 <hr/>
-
-                <div className={"compare"}>
-                    <div className={"two"}>
-                    <div className={"one"}>
-                    <p>teste</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>teste</p>
-                    </div>
-                    <div className={"one"}>
-                    <p>100</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>100</p>
-                    </div>
-                    </div>
-                    <div className={"two"}>
-                    <div className={"one"}>
-                    <p>111</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>111</p>
-                    </div>
-                    <div className={"one"}>
-                    <p>fá</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>sol</p>
-                    </div>
-                    </div>
-                    <div className={"two"}>
-                    <div className={"one"}>
-                    <p>111</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>111</p>
-                    </div>
-                    <div className={"one"}>
-                    <p>fá</p>
-                    <div className={'ryze'}>
-                        <p className={'legend'}>Quality of life index</p>
-                    <hr></hr>
-                    </div>
-                    <p>sol</p>
-                    </div>
-                    </div>
-                    
-                </div>
-
+<Compare></Compare>
             </div>
-
         )
-
     }
-}
+
 
 export {Comparison}
