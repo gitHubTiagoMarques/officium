ea52124a6762278919e37ed86345836d7b040e5c
cerkeira <ricardocerqueira0@protonmail.com>
2023-01-15 05:26:27 +0000

    almost  done

--- src/pages/Job_Finder.js
+++ src/pages/Job_Finder.js
@@ -12,7 +12,6 @@ function Finder() {
         <Navv></Navv>
           <div className={'resultss'}>
               <Search></Search>
-              {/* <Cities></Cities> */}
           </div>
         <Footer></Footer>
         
